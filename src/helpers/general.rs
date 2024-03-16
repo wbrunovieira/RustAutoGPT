2ee94d8e6ecb880c47821560d4b2112be112bc0d
wbrunovieira <wbrunovieira77@gmail.com>
2024-03-16 12:40:19 -0300

    ai function decoded

--- src/helpers/general.rs
+++ src/helpers/general.rs
@@ -1,3 +1,5 @@
+use serde::de::DeserializeOwned;
+
 use crate::models::general::llm::Message;
 use crate::helpers::command_line::PrintCommand;
 
@@ -50,6 +52,20 @@ pub async fn ai_task_request(
 
   
 
+}
+
+// Performs call to LLM GPT - Decoded
+pub async fn ai_task_request_decoded<T: DeserializeOwned>(
+  msg_context: String,
+  agent_position: &str,
+  agent_operation: &str,
+  function_pass: for<'a> fn(&'a str) -> &'static str,
+) -> T {
+  let llm_response: String =
+      ai_task_request(msg_context, agent_position, agent_operation, function_pass).await;
+  let decoded_response: T = serde_json::from_str(llm_response.as_str())
+      .expect("Failed to decode ai response from serde_json");
+  return decoded_response;
 }
 
 
