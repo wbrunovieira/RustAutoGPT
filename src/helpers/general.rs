e1820fb90911fa38449e51547be5725cfd6d206a
wbrunovieira <wbrunovieira77@gmail.com>
2024-03-16 12:32:35 -0300

    AI Function Task

--- src/helpers/general.rs
+++ src/helpers/general.rs
@@ -1,4 +1,7 @@
 use crate::models::general::llm::Message;
+use crate::helpers::command_line::PrintCommand;
+
+use crate::apis::call_request::call_gpt;
 
 // Extend ai function to encourage specific output
 pub fn extend_ai_function(ai_func: fn(&str) -> &'static str, func_input: &str) -> Message {
@@ -19,6 +22,35 @@ pub fn extend_ai_function(ai_func: fn(&str) -> &'static str, func_input: &str) -
         content: msg,
     }
 }
+// Performs call to LLM GPT
+pub async fn ai_task_request(
+  msg_context: String,
+  agent_position: &str,
+  agent_operation: &str,
+  function_pass: for<'a> fn(&'a str) -> &'static str,
+) -> String {
+  // Extend AI function
+  let extended_msg: Message = extend_ai_function(function_pass, &msg_context);
+
+   // Print current status
+   PrintCommand::AICall.print_agent_message(agent_position, agent_operation);
+
+     // Get LLM response
+     let llm_response_res: Result<String, Box<dyn std::error::Error + Send>> =
+     call_gpt(vec![extended_msg.clone()]).await;
+
+       // Return Success or try again
+    match llm_response_res {
+      Ok(llm_resp) => llm_resp,
+
+      Err(_) => call_gpt(vec![extended_msg.clone()])
+          .await
+          .expect("Failed twice to call OpenAI"),
+  }
+
+  
+
+}
 
 
 
@@ -36,5 +68,23 @@ mod tests {
         assert_eq!(extended_msg.role, "system".to_string());
     }
 
+    #[tokio::test]
+    async fn tests_ai_task_request() {
+        let ai_func_param: String =
+            "Build me a webserver for a simple payment requests.".to_string();
+
+        let res: String = ai_task_request(
+            ai_func_param,
+            "Managing Agent",
+            "Defining user requirements",
+            convert_user_input_to_goal,
+        )
+        .await;
+
+      assert!(res.len() > 20);
+
+    }
+
 
 }
+
